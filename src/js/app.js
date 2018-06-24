c530e6f64e2361faac9c8c4f8e6a89666636e87b
Jaylen Guevara-Kirksey Bey <36jguevara@gmail.com>
2018-06-24 13:24:16 -0400

    update1

---
+++ src/js/app.js
@@ -0,0 +1,6 @@
+function clickFunction1() {
+	document.getElementById("myDropDown1").classList.toggle("show1");
+}
+function clickFunction2() {
+	document.getElementById("myDropDown2").classList.toggle("show2");
+}
