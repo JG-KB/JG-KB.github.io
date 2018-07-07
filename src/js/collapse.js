69f3c9bed0fe0d089cd75604c1d6a1678364230b
Jaylen Guevara-Kirksey Bey <36jguevara@gmail.com>
2018-07-07 15:27:40 -0400

    Update 2.0

---
+++ src/js/collapse.js
@@ -0,0 +1,15 @@
+const showButton = document.getElementById('showButton');
+
+showButton.addEventListener('click',function(){
+  const para = document.querySelector('.textBoxP');
+
+  if (para.classList.contains('open')) {
+    para.classList.remove('open');
+    showButton.innerText = 'Show';   
+  } else { 
+    para.classList.add('open');
+    showButton.innerText = 'Hide';
+  }
+
+  open = !open; 
+});
---
+++ src/js/textLink.js
@@ -0,0 +1,28 @@
+document.querySelector("textarea").addEventListener("keyup", function() {
+    const div = document.querySelector(".jaylenCoding")
+
+    if (this.value !== "") {
+        div.innerHTML = this.value;
+
+        if(this.value.toLowerCase() === "about me") {
+            window.location.href="aboutMe.html";                            }
+
+        if(this.value.toLowerCase() === "github") {
+            window.open("https://github.com/JG-KB", "_blank");
+        }
+        if(this.value.toLowerCase() === "linkedin") {
+            window.open("https://www.linkedin.com/in/jaylen-guevara-kirksey-bey-098135165/", "_blank");
+        }
+        if(this.value.toLowerCase() === "email jaylen") {
+            window.open("mailto:36jguevara@gmail.com", "_blank");
+        }
+        if(this.value.toLowerCase() === "projects") {
+            window.location.href="projects.html";
+        }
+        }else{
+            div.innerHTML= " Hello ! Type where you want to go! use the keywords located at the bottom of the page";
+        }
+    });
+        
+    //    myfunction();
+    //    timeoutfunction();
